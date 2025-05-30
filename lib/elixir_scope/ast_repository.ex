ccaeb285358abbb1b782255c56075c6279e449fd
NSHkr <ZeroTrust@NSHkr.com>
2025-05-29 18:01:28 -1000

    Initial commit: Setup for ElixirScope.ASTRepository library and DESIGN.md

---
+++ lib/elixir_scope/ast_repository.ex
@@ -0,0 +1,18 @@
+defmodule ElixirScope.ASTRepository do
+  @moduledoc """
+  Documentation for `ElixirScope.ASTRepository`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirScope.ASTRepository.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_scope/ast_repository_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirScope.ASTRepositoryTest do
+  use ExUnit.Case
+  doctest ElixirScope.ASTRepository
+
+  test "greets the world" do
+    assert ElixirScope.ASTRepository.hello() == :world
+  end
+end
